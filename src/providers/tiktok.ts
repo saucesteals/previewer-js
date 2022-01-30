90542947f5a8bd932fbfaf87ca5011d18dc9fef9
Daniel <saucesteals@gmail.com>
2022-01-29 19:33:41 -0800

    please stop it tiktok

--- src/providers/tiktok.ts
+++ src/providers/tiktok.ts
@@ -8,18 +8,10 @@ const USER_AGENT =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36";
 
 const BASE_TIKTOK_HEADERS = {
-  accept:
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-  "accept-encoding": "gzip, deflate",
-  "accept-language": "en-US,en;q=0.9",
-  "cache-control": "max-age=0",
-  "sec-fetch-dest": "document",
-  "sec-fetch-mode": "navigate",
-  "sec-fetch-site": "none",
-  "sec-fetch-user": "?1",
-  "sec-gpc": "1",
-  "upgrade-insecure-requests": "1",
-  "user-agent": USER_AGENT,
+  "User-Agent": USER_AGENT,
+  Accept: "*/*",
+  "Accept-Encoding": "gzip, deflate",
+  Connection: "keep-alive",
 };
 
 const VIDEO_TIKTOK_HEADERS = {
@@ -79,10 +71,7 @@ export default class TiktokProvider extends BaseProvider {
   ): Promise<string | undefined> {
     const resp: AxiosResponse<string> = await this.http({
       url: videoUrl,
-      headers: {
-        ...BASE_TIKTOK_HEADERS,
-        "user-agent": USER_AGENT,
-      },
+      headers: BASE_TIKTOK_HEADERS,
     });
 
     const playAddrMatch = TiktokMatch.PlayAddr.exec(resp.data)?.[1];
