963ceaecab75d672ef8c1aae59a850abbb4df714
Daniel <saucesteals@gmail.com>
2022-05-18 07:07:21 -0700

    chrome ua with chrome replaced

--- src/providers/tiktok.ts
+++ src/providers/tiktok.ts
@@ -3,20 +3,30 @@ import { Message, MessageOptions } from "discord.js";
 import Stream from "stream";
 import BaseProvider from "../structures/provider";
 
-const USER_AGENT = "Pinterest/0.2 (+https://www.pinterest.com/bot.html)";
+// This is a chrome UA (version 101.0.4951.64) with "Chrome" replaced with a single (random) char
+// Discussed in: https://github.com/saucesteals/previewer/issues/22#issuecomment-1130030089
+const USER_AGENT =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) A/101.0.4951.64 Safari/537.36";
 
 const BASE_TIKTOK_HEADERS = {
-  "user-agent": USER_AGENT,
-  Connection: "keep-alive",
-  "Upgrade-Insecure-Requests": "1",
-  Accept:
+  accept:
     "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-  "Sec-GPC": "1",
-  "Sec-Fetch-Site": "none",
-  "Sec-Fetch-Mode": "navigate",
-  "Sec-Fetch-User": "?1",
-  "Sec-Fetch-Dest": "document",
-  "Accept-Language": "en-US,en;q=0.9",
+  "accept-language": "en-US,en;q=0.9",
+
+  "accept-encoding": "gzip, deflate", // no brotli
+  // axios has no built-in support for brotli
+  // and since it works without it - it's fine to just leave it out
+
+  connection: "keep-alive",
+  "cache-control": "max-age=0",
+  dnt: "1",
+  "sec-fetch-dest": "document",
+  "sec-fetch-mode": "navigate",
+  "sec-fetch-site": "none",
+  "sec-fetch-user": "?1",
+  "sec-gpc": "1",
+  "upgrade-insecure-requests": "1",
+  "user-agent": USER_AGENT,
 };
 
 const VIDEO_TIKTOK_HEADERS = {
